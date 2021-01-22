e0cff576df520cdd8d319ce219e6d67627288959
Burhanuddin Ahmed <burhanahmeed@gmail.com>
2021-01-22 09:57:18 +0700

    change display

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" width="25%" />
-    <p v-for="i in 10">AKAKAKAKAKAAKAKAK</p>
+    <p v-for="i in 10" :key="i">scroll down</p>
     <ContainerModal
       name="modal1"
       :footerOptions="{
@@ -20,8 +20,8 @@
       </template>
       <div>content</div>
     </ContainerModal>
-    <button @click="open">Click Here</button>
-    <p v-for="i in 10">HOHOHO</p>
+    <button @click="open" class="button">Click Here</button>
+    <p v-for="i in 10">scroll up</p>
   </div>
 </template>
 
@@ -60,5 +60,29 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
   position: relative;
+}
+.button {
+  margin: 0 5px;
+  display: inline-block;
+  font-weight: 400;
+  text-align: center;
+  white-space: nowrap;
+  vertical-align: middle;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+  border: 1px solid transparent;
+  border-top-color: transparent;
+  border-right-color: transparent;
+  border-bottom-color: transparent;
+  border-left-color: transparent;
+  padding: 0.375rem 0.75rem;
+  font-size: 0.9rem;
+  line-height: 1.5;
+  border-radius: 0.25rem;
+  transition: color 0.15s ease-in-out, background-color 0.15s ease-in-out,
+    border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
+  cursor: pointer;
 }
 </style>
