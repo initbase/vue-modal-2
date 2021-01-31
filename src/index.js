83a265fafac317395be25a3b7909e05d3754f06a
burhan ahmed <burhanahmeed@gmail.com>
2021-01-31 14:29:22 +0700

    unfix

--- src/index.js
+++ src/index.js
@@ -1,22 +1,29 @@
 import ContainerModal from "./components/Container.vue";
 import $eventBus from './Event.js'
+import { isVue2 } from "vue-demi";
 
 const optsDefault = {
   componentName: "vue-modal-2"
 };
 const plugin = {
   install(app, opts = {}) {
-    if (app.config.globalProperties.$vm2) {
-      return;
+    if (isVue2) {
+      if (app.prototype.$vm2) {
+        return;
+      }
+    } else {
+      if (app.config.globalProperties.$vm2) {
+        return;
+      }
     }
 
-    const options = Object.assign(optsDefault, opts);
+    let options = Object.assign(optsDefault, opts);
 
     const root = $eventBus;
 
     app.component(options.componentName, ContainerModal);
 
-    app.config.globalProperties.$vm2 = {
+    const api = {
       open: (name = "modal-1") => {
         root.trigger("toggle", {
           status: true,
@@ -29,8 +36,19 @@ const plugin = {
           name
         });
       },
-      root: root
-    };
+    }
+
+    if (isVue2) {
+      app.prototype.$vm2 = {
+        ...api,
+        root: root
+      };
+    } else {
+      app.config.globalProperties.$vm2 = {
+        ...api,
+        root: root
+      };
+    }
   }
 };
 
